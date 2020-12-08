bd75207b1a8515a9b7760b7bd2decfb3616e18ca
Eugen Chernitsky <EugenChernitsky@coherentsolutions.com>
2020-12-08 15:58:11 +0300

    Initial Commit

---
+++ TestXcode/TestXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXcodeApp.swift
+//  TestXcode
+//
+//  Created by Eugen Chernitsky on 12/8/20.
+//
+
+import SwiftUI
+
+@main
+struct TestXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
